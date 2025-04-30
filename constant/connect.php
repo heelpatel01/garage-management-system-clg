ba810550d9f2bee9af776f9bdc00f106e4508750
HEEL PATEL <heelpatel216@gmail.com>
2025-04-30 22:08:19 +0530

    first commit

---
+++ constant/connect.php
@@ -0,0 +1,30 @@
+<?php 
+// DB credentials.
+$localhost = "localhost";
+$username = "root";
+$password = "";
+$dbname = "garage_system";
+//$store_url = "http://localhost/'Folder Name'/";
+// db connection
+$connect = new mysqli($localhost, $username, $password, $dbname);
+// check connection
+if($connect->connect_error) {
+  die("Connection Failed : " . $connect->connect_error);
+} else {
+  // echo "Successfully connected";
+}
+?>
+
+<!-- /*!
+* Author Name: MH RONY.
+* GigHub Link: https://github.com/dev-mhrony
+* Facebook Link:https://www.facebook.com/dev.mhrony
+* Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
+for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com
+* Visit My Website : developerrony.com
+*/ -->
+
+
+
+
+
